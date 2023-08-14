08251a6ac257568b1a81549e5f3cad2793d463f1
adihakanyan <93980513+adihakanyan@users.noreply.github.com>
2023-08-14 20:39:42 +0300

    Commit 4- Adi : server- main ,scrapper, server
    server - create a server that listen for text and if the text is ticker symbol it will send the telegram bot info about the stock according to the function send_info in scrapper
    
    scrapper - modify to much to server file
    
    main - starting the server and rum it

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
-from scrapper import print_info
+from server import updater
 
 
-# TODO: write a try  except code for entering incorrect ticker_symbol  -  complete
-# TODO: modularize main function  -  complete
-
-def get_ticker_symbol():
-    return input('Please enter a ticker symbol (to quit press "q"): ')
-
-
-def handle_exceptions(exception):
-    error_messages = {
-        KeyError: "A key error occurred. Please try again.",
-        IndexError: "An index error occurred. Please try again.",
-        ConnectionError: "A connection error occurred. Please try again.",
-        TimeoutError: "The request timed out. Please try again.",
-        ValueError: "An invalid value was provided. Please try again.",
-        Exception: f"An unexpected error occurred: {e}. Please try again."
-    }
-    print(error_messages.get(type(exception), f"An unexpected error occurred: {exception}. Please try again."))
+def main():
+    print("Starting bot...")
+    updater.start_polling()
+    updater.idle()
+    print("Bot stopped.")
 
 
-if __name__ == '__main__':
-    run = True
-    while run:
-        try:
-            ticker_symbol = get_ticker_symbol()
-            if ticker_symbol == 'q':
-                print("See you next time :)")
-                run = False
-                break
-            print_info(ticker_symbol)
-        except (KeyError, IndexError, ConnectionError, TimeoutError, ValueError, Exception) as e:
-            handle_exceptions(e)
+if __name__ == "__main__":
+    main()
--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,5 @@
-# TODO: write a rty except code for entering incorrect ticker_symbol  -  complete
-# TODO: Cancel the message pycharm send when an error accord (not so important for the final project) - already try
-#          try:
-#              with warnings.catch_warnings():
-#                 warnings.simplefilter("ignore")
-# TODO: Add code to print_info function, so that it will also print the candlestick chart of the stock  -  complete
-# TODO: modularize print_info  -  complete
+# TODO: modify the code to mach the server.py file  - complete
+
 
 import yfinance as yf
 import mplfinance as mpf
@@ -43,27 +38,26 @@ def create_text_message(ticker_symbol, current_price, open_price, close_price, h
     return text_message
 
 
-def create_chart(historical_data, ticker_symbol):
+def create_chart(historical_data, ticker_symbol, chart_file_name):
     # Create the candlestick chart and print it to the screen
-    mpf.plot(historical_data, type='candle', style='charles', title=f'{ticker_symbol} Stock Price')
+    mpf.plot(historical_data, type='candle', style='charles', title=f'{ticker_symbol} Stock Price', savefig=chart_file_name)
 
 
 def handle_exceptions(exception):
     print("scrapper file - exception")
-    raise exception
+    return "כנראה שהייתה טעות בשם המניה.. נסה שוב :) ", None
 
 
-def print_info(ticker_symbol):
+def send_info(ticker_symbol):
     try:
         ticker_symbol_upper = upper_symbol_ticker(ticker_symbol)
         ticker = create_ticker_object(ticker_symbol_upper)
         historical_data = get_ticker_data(ticker)
         current_price, open_price, close_price, high_price, low_price = extract_recent_data(historical_data)
         text_message = create_text_message(ticker_symbol_upper, current_price, open_price, close_price, high_price,low_price)
-        print(text_message)
-        create_chart(historical_data, ticker_symbol_upper)
+        return text_message, historical_data
 
     # re-raise the exception to calling code
     except (KeyError, IndexError, ConnectionError, TimeoutError, ValueError, Exception) as e:
-        handle_exceptions(e)
+        return handle_exceptions(e)
 
