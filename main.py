8154a7fd365710dec39b6a6b687d324db814d2e4
adihakanyan <93980513+adihakanyan@users.noreply.github.com>
2023-08-16 12:41:24 +0300

    Enhanced debugging for bot startup and operation

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from server import updater
 
-
 def main():
     print("Starting bot...")
-    updater.start_polling()
-    updater.idle()
+    try:
+        updater.start_polling()
+        print("Bot polling started...")
+        updater.idle()
+    except Exception as e:
+        print(f"Error: {e}")
     print("Bot stopped.")
-
 
 if __name__ == "__main__":
     main()
